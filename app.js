4af0da76cbf81986d652804f737171f38e2ec1e7
benito ESTEBAN-BODIPO <besteba@c3r1s7.wethinkcode.co.za>
2018-11-30 22:18:38 +0200

    Changes

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-var express = require('express'),
-	app = express();
+var express = require('express');
+var app = express();
 
-var index = require('./controllers/index-controller'),
-	 profile = require('./controllers/home-controller'),
-	 profile = require('./controllers/profile-controller'),
-	 inbox = require('./controllers/inbox-controller'),
-	 user	= require('./controllers/user-controller');
+app.set('view engine', 'ejs');
+app.use("/public", express.static("public"));
+
+var index = require('./controllers/index-controller');
+// 	 profile = require('./controllers/home-controller'),
+// 	 profile = require('./controllers/profile-controller'),
+// 	 inbox = require('./controllers/inbox-controller'),
+// 	 user	= require('./controllers/user-controller');
+
+app.get('/', function(req, res){
+	var content = {
+		title: "Matcha | Welcome",
+		css: ["home"],
+		js: ["slider"]
+	};
+	res.render('index', content);
+})
 
 
-app.listen(3000, function(){ console.log("runing on port 8000");});
+
+
+
+app.listen(8080, function(){ console.log("runing on port 8080");});
